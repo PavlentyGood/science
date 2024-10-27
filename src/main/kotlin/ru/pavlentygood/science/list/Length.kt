0cef756ec2115cd0559c941412ed8758cfe5848f
pavlentygood <pavlentygood@yandex.ru>
2024-10-28 00:18:26 +0300

    length

---
+++ src/main/kotlin/ru/pavlentygood/science/list/Length.kt
@@ -0,0 +1,13 @@
+package ru.pavlentygood.science.list
+
+fun length1(list: List<*>?): Int =
+    if (list == null) 0
+    else 1 + length1(list.tail)
+
+fun length2(list: List<*>?): Int {
+    tailrec fun lengthIter(list: List<*>?, n: Int): Int =
+        if (list == null) n
+        else lengthIter(list.tail, n + 1)
+
+    return lengthIter(list, 0)
+}
---
+++ src/test/kotlin/ru/pavlentygood/science/list/LengthKtTest.kt
@@ -0,0 +1,18 @@
+package ru.pavlentygood.science.list
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class LengthKtTest {
+
+    @Test
+    fun length() {
+        length1(null) shouldBe 0
+        length1(List(1, null)) shouldBe 1
+        length1(List(1, List(1, null))) shouldBe 2
+
+        length2(null) shouldBe 0
+        length2(List(1, null)) shouldBe 1
+        length2(List(1, List(1, null))) shouldBe 2
+    }
+}
